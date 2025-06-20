6d739c6062f9f98291194e279571c3088a6590e6
Madan Shahi <fullstackshahi@gmail.com>
2025-06-20 16:33:00 +0530

    production level jwt

--- API/src/utils/JWT.ts
+++ API/src/utils/JWT.ts
@@ -1,13 +1,20 @@
-import jwt from "jsonwebtoken";
+import jwt, { SignOptions } from "jsonwebtoken";
 import { promisify } from "util";
 import { ConfigError, ConfigP } from "../errors/CustomErrors";
 import { logger } from "./Logger";
 
 export const token = (payload: object) => {
-  if (!process.env.JWT_SECRET) {
+  const secret = process.env.JWT_SECRET;
+  const expiresIn = parseInt(process.env.JWT_EXPIRES_IN) || "90d";
+  if (!secret) {
     logger.error(`JWT Secret is not configured`, 500);
   }
-  return jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN || "90d",
-  });
+  const options: SignOptions = { expiresIn };
+
+  return jwt.sign(payload, secret, options);
+};
+
+export const verifyToken = async (token: string) => {
+  const verifyAsync = promisify(jwt.verify);
+  return await verifyAsync.call(jwt, token, process.env.SECRET);
 };
